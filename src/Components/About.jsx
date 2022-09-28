23d3c6eeb819c1063a44268ea3e562ed0e5354bb
harshalpatil655 <harshalpatil655@gmail.com>
2022-09-28 17:36:43 +0530

    Addedaboutnew

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -6,47 +6,59 @@ const About = () => {
   return (
     <div className={styles.about} id={"about"}>
       <div className={styles.toptitle}>
-        <h1 style={{ textDecoration: "underline" }}>About Me</h1>
-        <p style={{ marginTop: "-18px", fontWeight: "600", color: "gray" }}>
-          My Introduction
-        </p>
+        <h1 className={styles.abtname}>About Me</h1>
+        <p className={styles.abtsubname}>My Introduction</p>
       </div>
-      <div className={styles.abtflex}>
-        <div>
-          <img className={styles.abtimage} src={photo} alt="Imageharshal" />
-        </div>
-        <div>
-          <div className={styles.abtpara}>
-            An enthusiastic fresher seeking a career with a progressive
-            organization to capitalize my skills and abilities in the ﬁeld of
-            information technology and eager to learn new technologies and
-            methodologies.
+      <div className={styles.abtdata}>
+        <div className={styles.abtflex}>
+          <div>
+            <img className={styles.abtimage} src={photo} alt="Imageharshal" />
           </div>
-          <div className={styles.abtnumbers}>
-            <div>
-              <h3>1200+</h3>
-              <p
-                style={{ marginTop: "-18px", fontWeight: "600", color: "gray" }}
-              >
-                hours of coding
-              </p>
+          <div>
+            <div className={styles.abtpara}>
+              An enthusiastic fresher seeking a career with a progressive
+              organization to capitalize my skills and abilities in the ﬁeld of
+              information technology and eager to learn new technologies and
+              methodologies.
             </div>
-            <div>
-              <h3>3+</h3>
-              <p
-                style={{ marginTop: "-18px", fontWeight: "600", color: "gray" }}
-              >
-                {" "}
-                website clone projects
-              </p>
-            </div>
-            <div>
-              <h3>5+</h3>
-              <p
-                style={{ marginTop: "-18px", fontWeight: "600", color: "gray" }}
-              >
-                Tech Stack
-              </p>
+            <div className={styles.abtnumbers}>
+              <div>
+                <h3>1200+</h3>
+                <p
+                  style={{
+                    marginTop: "-18px",
+                    fontWeight: "600",
+                    color: "gray",
+                  }}
+                >
+                  hours of coding
+                </p>
+              </div>
+              <div>
+                <h3>5+</h3>
+                <p
+                  style={{
+                    marginTop: "-18px",
+                    fontWeight: "600",
+                    color: "gray",
+                  }}
+                >
+                  {" "}
+                  website clone projects
+                </p>
+              </div>
+              <div>
+                <h3>10+</h3>
+                <p
+                  style={{
+                    marginTop: "-18px",
+                    fontWeight: "600",
+                    color: "gray",
+                  }}
+                >
+                  Tech Stack
+                </p>
+              </div>
             </div>
           </div>
         </div>
--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import styles from "../CSS/Home.module.css";
 import { AiFillLinkedin, AiTwotoneMail, AiFillGithub } from "react-icons/ai";
-import photo from "../Image/harshalphoto.jpg";
+
 import myresume from "../Resume/myresume.pdf";
 
 const Home = () => {
@@ -12,8 +12,8 @@ const Home = () => {
   return (
     <div className={styles.home} id={"home"}>
       <div className={styles.homeflex}>
-        <div style={{ color: "#018ec4" }}>
-          <div className={styles.weblink}>
+        <div className={styles.weblink}>
+          <div>
             {" "}
             <a
               href="https://www.linkedin.com/in/harshalpatil655"
@@ -56,44 +56,25 @@ const Home = () => {
         </div>
         <div className={styles.taketotop}>
           <div>
-            <h1 className={styles.myh1}>
-              {" "}
-              <span
-                style={{
-                  fontSize: "40px",
-                  marginLeft: "-300px",
-                }}
-              >
-                Hi,
-              </span>
-            </h1>
-            <h1
-              className={styles.myname}
-              style={{ marginTop: "-20px", textAlign: "start" }}
-            >
-              I am Harshal Patil
+            <h1 className={styles.text}>Hello, 👋 </h1>
+            <h1 className={styles.myname}>
+              My name is <span className={styles.name}>Harshal Patil</span>
             </h1>
           </div>
           <div className={styles.titleitemwrapper}>
-            <h4 className={styles.titleitem} style={{ marginTop: "-20px" }}>
-              Full Stack Web Developer
-            </h4>
+            <h4 className={styles.titleitem}>Full Stack Web Developer</h4>
           </div>
           <div>
-            <p className={styles.myp} style={{ marginTop: "-10px" }}>
-              Skilled in react and redux and capable of making any kind of
-              website{" "}
+            <p className={styles.homep}>
+              Passionate about learning new technologies, and Interested in
+              creating web applications & providing solutions to a real-world
+              problems.
             </p>
           </div>
           <div>
             <a href={myresume} target="_blank" rel="noreferrer" download>
               <button className={styles.homebtn}> Download Resume</button>
             </a>
-          </div>
-        </div>
-        <div>
-          <div>
-            {/* <img className={styles.myimg} src={photo} alt="Harshal Patil" /> */}
           </div>
         </div>
       </div>
